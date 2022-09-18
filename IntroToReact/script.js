19f945eb1098d927e4170fb3be8d2f999c91d3cd
Masha-Aldossari <102030206+Masha-Aldossari@users.noreply.github.com>
2022-09-18 15:48:41 +0300

    Add files via upload

---
+++ IntroToReact/script.js
@@ -0,0 +1,28 @@
+// هنا ربط الجافاسكريبت بالدوم او ماتسمى فانيلا جافاسكريبت
+
+// const name1 = document.getElementById("root");
+// const element = document.createElement("div");
+// element.innerHTML = "HelloWorld";
+// name1.appendChild(element);
+
+// التحويل من جافاسكريبت الى رياكت
+
+// const element = React.createElement('div' , {
+//       children : "Hello",
+//     //   className : "container"
+// })
+
+
+
+// JSX
+
+// let container = document.getElementById("root")
+
+// const element = (
+//   <div>
+//     <h1>Hello World</h1>
+//   </div>
+// );
+// ReactDOM.render(element, container);
+
+console.log("hello");
